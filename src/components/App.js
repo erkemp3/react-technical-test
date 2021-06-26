48a0ce8964615ccb72b03cd794c315443bb632cc
Ellie Kemp <elliekemp95@gmail.com>
2021-06-26 13:07:45 +0100

    Initial setup complete, Search component added and initial tests passing

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/App.css";
+import Search from "./Search.js";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="NASA Logo"
+        className="nasa-img"
+      />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
