00cac24e53a24450acb9edb6772c7b76a67c45fd
Ellie Kemp <elliekemp95@gmail.com>
2021-06-26 13:31:00 +0100

    Search button added and logic implemented

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,20 @@ import React, { useState } from "react";
 import "../styles/Search.css";
 
 const Search = () => {
+  const [value, setValue] = useState("");
+
   return (
     <>
-      <input type="text" className="search-input" />
+      <form className="search-form">
+        <input
+          type="text"
+          className="search-form__input"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="search-form__button" type="submit">
+          Search
+        </button>
+      </form>
     </>
   );
 };
