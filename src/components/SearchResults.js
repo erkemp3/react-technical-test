a5158f74e1cce1a3bdeb5acee2932817bcfd4314
Ellie Kemp <elliekemp95@gmail.com>
2021-06-26 17:19:17 +0100

    multiple image mapping functionality added, styling added

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,12 +7,12 @@ const SearchResults = ({ results }) => {
   }
   return (
     <>
-      <p>Search Results</p>
-      <img
-        className="data-image"
-        src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        alt="space pic"
-      />
+      <p>Search Results:</p>
+      <div className="image-container">
+        {results.map((image) => (
+          <img className="data-image" src={image} alt="space pics" />
+        ))}
+      </div>
     </>
   );
 };
